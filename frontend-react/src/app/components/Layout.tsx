aca50c29737839c666fb01db654c0ec94f1570bd
Mathis MAURY <mathis.maury@limayrac.fr>
2026-06-18 10:58:22 +0200

    création d'une fonction qui permet que lorsqu'on clique sun lien dans le footer ca nous ramène en haut de la page pour pouvoir la consulter

--- frontend-react/src/app/components/Layout.tsx
+++ frontend-react/src/app/components/Layout.tsx
@@ -1,11 +1,19 @@
-import { Outlet } from 'react-router';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router';
 import { Navbar } from './Navbar';
 import { Footer } from './Footer';
 import { ChatBot } from './ChatBot';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => { window.scrollTo(0, 0); }, [pathname]);
+  return null;
+}
+
 export function Layout() {
   return (
     <div className="min-h-screen flex flex-col bg-[#0A1628]">
+      <ScrollToTop />
       <Navbar />
       <main className="flex-1">
         <Outlet />
